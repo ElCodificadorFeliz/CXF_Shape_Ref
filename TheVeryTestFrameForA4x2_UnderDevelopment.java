b1aa2a684ec60d7a58ef6f779ceebaaf7dada1a8
Michael Schaefers <Michael.Schaefers@HAW-Hamburg.de>
2015-11-18 08:28:28 +0100

    adding explanations about construction of cuboids

--- TheVeryTestFrameForA4x2_UnderDevelopment.java
+++ TheVeryTestFrameForA4x2_UnderDevelopment.java
@@ -65,7 +65,7 @@ public class TheVeryTestFrameForA4x2_UnderDevelopment {
             new Point( -8.3,  +4.9,  +5.3),   new Point( -2.3,  -2.1,  -0.7),   new Point( +0.7, +10.9,  +7.3),   new Point( +6.7,  +3.9,  +1.3),
             new Point( -6.3,  -1.1, +14.3),   new Point( -0.3,  -8.1,  +8.3),   new Point( +2.7,  +4.9, +16.3),   new Point( +8.7,  -2.1, +10.3)
         );
-        //  ( -8.3, +4.9, +5.3 )    "+"    (+6.0, -6.0, -6.0),  (+1.0, -8.0, +4.0),  (-8.0, +1.0, +4.0)
+        //  ...
         
         // Cube
         permutate(
@@ -87,23 +87,23 @@ public class TheVeryTestFrameForA4x2_UnderDevelopment {
         );
         // ...
         
-        // Cuboid (?)
+        // Cuboid
         permutate(
             false,                  // cube expected ?
             true,                   // cuboid expected ?
             new Point( -3.7,  -1.7,  -4.3),   new Point( -0.7, -25.7,  +7.7),   new Point(+19.3,  -5.7, +42.7),   new Point(-16.7, -23.7, +15.7),
             new Point(+16.3, +18.3, +30.7),   new Point(-19.7,  +0.3,  +3.7),   new Point( +0.3, +20.3, +38.7),   new Point(  3.3,  -3.7, +50.7)
         );
-        // ...
+        //  ( -3.7, -1.7, -4.3 )    "+"  5*(+4.0, +4.0, +7.0),  3*(+1.0, -8.0, +4.0),  2*(-8.0, +1.0, +4.0)
         
-        // Cuboid (?)
+        // Cuboid
         permutate(
             false,                  // cube expected ?
             true,                   // cuboid expected ?
             new Point( -4.9,  +0.8,  -0.1),   new Point( -0.9,  -2.7,  -1.1),   new Point( +3.1,  +4.3,  -0.6),   new Point( -1.9,  -0.7,  -3.1),
             new Point( +4.1,  +2.3,  +1.4),   new Point( +0.1,  +5.8,  +2.4),   new Point( -3.9,  -1.2,  +1.9),   new Point( +1.1,  +3.8,  +4.4)
         );
-        // ...
+        //  ( -1.9, -0.7, -3.1 )    "+"  2.5*(+2.0, +2.0, +1.0),  (+1.0, -2.0, +2.0),  1.5*(-4.0, +2.0, +4.0)
         
         
         
